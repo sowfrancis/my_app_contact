25f9a53b54301cc244e7bbbcd4a5d7b05fbacc26
sowfrancis <sow.francis@gmail.com>
2016-10-27 19:13:20 +0200

    ajout method import index et routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,11 @@
 class ContactsController < ApplicationController
+	def index
+		@contacts_without_error = Contact.where(record_error: nil)
+		@contacts_with_error = Contact.where.not(record_error: nil)
+	end
+
+	def import
+		@contact = Contact.import(params[:file])
+  	redirect_to root_url, notice: "Contact imported."
+	end
 end
